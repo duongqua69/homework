2ed4d2823a303d920efc1709508a776794274207
tungduong <duongday69@gmail.com>
2024-10-10 18:54:44 +0700

    Finish version

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int ADCvalue = analogRead(LDR_PIN);
+  Serial.print(">ADCvalue:");
+  Serial.println(ADCvalue);
+  delay(100);
+}
